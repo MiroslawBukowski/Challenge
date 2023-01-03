c52c5778342271bfa432e80771b756a3ce17e622
MiroslawBukowski <miroslawb@onet.eu>
2023-01-03 22:23:36 +0100

    W21_dzień 3 - wersja aplikacji zwracająca łańcuch z tablicy string

---
+++ src/ChallengeApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+namespace ChallengeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            //#error version
+            WriteLine($"Hello {args[0]}");// System.Console.WriteLine("Biorę udział w Wyzwaniu Programistycznym wyzwanie21.pl! Oto zadanie domowe dzień 2");
+        }
+    }
+}
